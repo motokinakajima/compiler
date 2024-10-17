0329063049085ce1985ffe0d78cf04b6db3d8818
motoki nakajima <motokinakajima0912@gmail.com>
2024-10-17 09:57:22 -0400

    added src files

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 中嶋幹 on 10/17/24.
+//
+
+#include "main.h"
+
+#include <cstdio>
+
+int main() {
+    printf("Hello World!\n");
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by 中嶋幹 on 10/17/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
